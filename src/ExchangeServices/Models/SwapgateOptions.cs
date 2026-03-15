958ec7f7cdf77c0247405d9d533648dec51df2f4
Ryan Williams <ryanmichaelwilliams@gmail.com>
2026-03-15 16:13:22 -0400

    swapgate

---
+++ src/ExchangeServices/Models/SwapgateOptions.cs
@@ -0,0 +1,17 @@
+namespace ExchangeServices.Implementations;
+
+public sealed class SwapgateOptions
+{
+    public string BaseUrl { get; set; } = "https://swapgate.io/";
+    public string SiteName { get; set; } = "Swapgate";
+    public string? SiteUrl { get; set; } = "https://swapgate.io";
+    public char PrivacyLevel { get; set; } = 'B';
+    public int RequestTimeoutSeconds { get; set; } = 10;
+
+    public string XmrCurrency { get; set; } = "XMR";
+    public string XmrNetwork { get; set; } = "XMR";
+    public string UsdtCurrency { get; set; } = "USDT";
+    public string UsdtNetwork { get; set; } = "TRC20";
+    public decimal SellProbeAmountXmr { get; set; } = 2m;    // must exceed Swapgate min (~1.566 XMR)
+    public decimal BuyProbeAmountUsdt { get; set; } = 100m;
+}
